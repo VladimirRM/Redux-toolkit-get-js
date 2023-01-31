2876006626bedc38c1ec51c8fd50d6f4921eb71e
Vladimir RM <shvdrummer@gmail.com>
2023-01-31 18:25:22 +0200

    import

---
+++ src/features/user/userSlise.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState ={
+    firstName:'',
+    lastName:'',
+}
+
+export const userSlice = createSlice({
+    name:'user',
+    initialState,
+    reducers: {
+        setFirstName: 
+            (state,action)=>{
+                state.firstName = action.payload
+            }
+        
+    }
+})
+
+export const {setFirstName}= userSlice.actions
+
+export default userSlice.reducer
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlise from "../features/user/userSlise";
+export const store = configureStore({
+    reducer:{
+        user: userSlise
+    }
+})
