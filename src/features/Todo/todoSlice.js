0e7bdb826a155a4a64f9f11cd2d0774d3bea5b41
Vladimir RM <shvdrummer@gmail.com>
2023-02-01 08:39:21 +0200

    import createSlice

---
+++ src/features/Todo/todoSlice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+const todoSlice = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default todoSlice
